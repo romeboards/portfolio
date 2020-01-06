065674c3b90fad704556a239a372c9b72fe6a55d
Dan Kass <romeboards@gmail.com>
2020-01-06 17:20:15 -0500

    initial build out + some content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Daniel Kass – Portfolio`,
+    description: `This is the description for my portfolio site.`,
+    author: `@dananalog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +14,24 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/src/fonts/`
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        fonts: [
+          {
+            family: `Libre Baskerville`,
+          },
+        ],
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,165 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <section className="hero">
+      <h2>
+        I’m an interdisciplinary coder, designer, and progressive advocate.
+      </h2>
+      <p>
+        My work supports social justice movements through the use of data and
+        technology. I design & build tools that investigate social and technical
+        systems, make institutions more accessible, and tell critical stories
+        through digital media.
+      </p>
+      <p>
+        For the past five years, I served as a co-founder and Executive Director
+        at{" "}
+        <a href="https://www.justfix.nyc" target="_blank">
+          JustFix.nyc
+        </a>
+        , a tech nonprofit that supports tenants and organizers fighting
+        displacement and neglectful housing in N.Y.C. We’ve successfully served
+        thousands of New Yorkers as well as brought critical transparency to
+        real estate industry practices. For this work, I was named on the Forbes
+        <em>“30 Under 30”</em> list in the law & policy category. I also serve
+        on the <a>Open Data Advisory Council</a> for the City of New York, the
+        steering committee for the <a>Housing Data Coalition</a>, and the
+        Emerging Leaders Council for the <a>Legal Services Corporation</a>, the
+        nation’s largest funder of legal aid organizations.
+      </p>
+      <p>
+        I have extensive experience in the digital industry and have produced
+        high-end digital experiences for clients such as Spotify, NPR, and the
+        Bill & Melinda Gates Foundation. I have a B.S. in Computer Science from
+        Tufts University and was an Engineering Fellow in the{" "}
+        <a>Blue Ridge Labs</a> fellowship program.
+      </p>
+      <p>
+        Currently, I'm seeking collaborative projects with individuals and
+        groups working in journalism, organizing, advocacy, and civic tech.{" "}
+        <a>Let's scheme.</a>
+      </p>
+    </section>
+    <section className="projects">
+      <h2>Selected Projects</h2>
+      <div className="projects--list">
+        <article className="project">
+          <h3>Who Owns What in NYC?</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+
+        <article className="project">
+          <h3>JustFix.nyc Tenant Web-app</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+
+        <article className="project">
+          <h3>Eviction Free NYC</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+
+        <article className="project">
+          <h3>JustFix.nyc SMS services</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+
+        <article className="project">
+          <h3>Localore: Finding America</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+
+        <article className="project">
+          <h3>Spotify Stranger Things</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+
+        <article className="project">
+          <h3>Random Other Project</h3>
+          <p>
+            Senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+            tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            Donec eu libero sit amet quam egestas semper.{" "}
+            <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
+            leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
+            erat wisi, condimentum sed, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui.{" "}
+            <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
+            felis.
+          </p>
+        </article>
+      </div>
+    </section>
+
+    <section className="links">
+      <h2>More Content</h2>
+    </section>
   </Layout>
 )
 
