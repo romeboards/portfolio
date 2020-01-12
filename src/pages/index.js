1baea8407a88f5343cbd71267a096b753c8f0571
Dan Kass <romeboards@gmail.com>
2020-01-11 23:00:18 -0500

    ok this should be it

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,11 @@ const IndexPage = () => (
       <p>
         For the past five years, I served as a co-founder and Executive Director
         at{" "}
-        <a href="https://www.justfix.nyc" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.justfix.nyc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           JustFix.nyc
         </a>
         , a tech nonprofit that supports tenants and organizers fighting
@@ -33,15 +37,27 @@ const IndexPage = () => (
         brought transparency to real estate industry practices. For this work, I
         was named on the Forbes “30 Under 30” list in the law & policy category.
         I also serve on the{" "}
-        <a href="https://opendata.cityofnewyork.us/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://opendata.cityofnewyork.us/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Open Data Advisory Council
         </a>{" "}
         for the City of New York, the steering committee for the{" "}
-        <a href="https://www.housingdatanyc.org/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.housingdatanyc.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Housing Data Coalition
         </a>
         , and the Emerging Leaders Council for the{" "}
-        <a href="https://www.lsc.gov/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.lsc.gov/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Legal Services Corporation
         </a>
         .
@@ -51,15 +67,24 @@ const IndexPage = () => (
         high-end digital projects for clients such as Spotify, NPR, and the Bill
         & Melinda Gates Foundation. I have a B.S. in Computer Science from Tufts
         University and was an Engineering Fellow in the{" "}
-        <a href="https://labs.robinhood.org/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://labs.robinhood.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Blue Ridge Labs
         </a>{" "}
         fellowship program.
       </p>
       <p>
-        Currently, I'm seeking collaborative data & storytelling projects with individuals and
-        groups working in journalism, organizing, advocacy, and civic tech.{" "}
-        <a href="mailto:sayheytodan@gmail.com" target="_blank" rel="noopener noreferrer">
+        Currently, I'm seeking collaborative data & storytelling projects with
+        individuals and groups working in journalism, organizing, advocacy, and
+        civic tech.{" "}
+        <a
+          href="mailto:sayheytodan@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Let's talk <span>&#10153;&#xFE0E;</span>
         </a>
       </p>
@@ -81,7 +106,12 @@ const IndexPage = () => (
                 )}
               </p>
               {proj.image && (
-                <a className="project--image" href={proj.url} target="_blank" rel="noopener noreferrer">
+                <a
+                  className="project--image"
+                  href={proj.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
                     src={require(`../images/${proj.image}`)}
                     alt={proj.title}
@@ -93,39 +123,42 @@ const IndexPage = () => (
         ))}
       </div>
     </section>
-    <section className="lists no-arrow">
+    <section className="lists">
       <div>
-        <h3>In the news</h3>
-        <ul>
-          {listsData.press.map((story, idx) => (
-            <li key={idx}>
-              {story.title}
-              {"   "}
-              <a href={story.url} target="_blank" rel="noopener noreferrer">
-                {story.outlet} &#x27AD;&#xFE0E;
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h3>Conference talks</h3>
-        <ul>
-          {listsData.confs.map((talk, idx) => (
-            <li key={idx}>
-              {talk.title}
-              <br />
-              <em>{talk.conf}</em>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h3>In the news</h3>
+          <ul>
+            {listsData.press.map((story, idx) => (
+              <li key={idx}>
+                {story.title}
+                {"   "}
+                <a href={story.url} target="_blank" rel="noopener noreferrer">
+                  {story.outlet} &#x27AD;&#xFE0E;
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>Conference talks</h3>
+          <ul>
+            {listsData.confs.map((talk, idx) => (
+              <li key={idx}>
+                {talk.title}
+                <br />
+                <em>{talk.conf}</em>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
 
     <section className="big-sky no-arrow">
       <a
         href="https://www.curbed.com/2015/9/2/9924926/hans-haacke-photography-slumlord"
-        target="_blank" rel="noopener noreferrer"
+        target="_blank"
+        rel="noopener noreferrer"
       >
         <PosterImage />
       </a>
@@ -136,7 +169,11 @@ const IndexPage = () => (
         &copy; Dan Kass, 2020
         <ul>
           <li>
-            <a href="mailto:sayheytodan@gmail.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="mailto:sayheytodan@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Email
             </a>
           </li>
@@ -146,19 +183,28 @@ const IndexPage = () => (
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/dananalog" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com/dananalog"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Twitter
             </a>
           </li>
           <li>
-            <a href="https://github.com/romeboards" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/romeboards"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Github
             </a>
           </li>
           <li>
             <a
               href="https://www.linkedin.com/in/dan-kass-1734b444/"
-              target="_blank" rel="noopener noreferrer"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Linkedin
             </a>
